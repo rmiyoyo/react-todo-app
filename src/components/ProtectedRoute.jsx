19042c0877913bc1c2bfafa6aeff346fb10da542
rmiyoyo <miyoyoraphael@gmail.com>
2023-09-30 13:18:08 +0300

    Add components

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,25 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuthContext } from '@/context/AuthContext';
+import PropTypes from 'prop-types';
+
+const ProtectedRoute = ({ children }) => {
+    const { user } = useAuthContext();
+    const location = useLocation();
+    console.log(location);
+    if (!user) {
+        return (
+            <Navigate
+                to="/login"
+                state={{ pathname: location.pathname }}
+                replace
+            />
+        );
+    }
+    return children;
+};
+
+ProtectedRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default ProtectedRoute;
